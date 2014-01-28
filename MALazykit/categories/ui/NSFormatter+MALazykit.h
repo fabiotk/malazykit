c485a9cffbec02768b69f57e200bb14236dfe1ed
Fábio Nogueira <fna.contact@gmail.com>
2014-01-28 15:14:41 -0200

    -  Adding Lazy Initialization to NSFormatter

---
+++ MALazykit/categories/ui/NSFormatter+MALazykit.h
@@ -0,0 +1,15 @@
+//
+//  NSFormatter+MALazykit.h
+//  MALazykit
+//
+//  Created by Fábio Almeida on 1/28/14.
+//  Copyright (c) 2014 Marlon Andrade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSFormatter (MALazykit)
+
++ (instancetype)formatter;
+
+@end
---
+++ MALazykit/categories/ui/NSFormatter+MALazykit.m
@@ -0,0 +1,17 @@
+//
+//  NSFormatter+MALazykit.m
+//  MALazykit
+//
+//  Created by Fábio Almeida on 1/28/14.
+//  Copyright (c) 2014 Marlon Andrade. All rights reserved.
+//
+
+#import "NSFormatter+MALazykit.h"
+
+@implementation NSFormatter (MALazykit)
+
++ (instancetype)formatter {
+    return [[self alloc] init];
+}
+
+@end
